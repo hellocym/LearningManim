cdae06d134d59bb01d1f22fba86ad73f4e0db545
hellocym <13681921379@163.com>
2022-11-18 12:16:18 +0000

    2

--- project/scene.py
+++ project/scene.py
@@ -1,14 +1,33 @@
 from manim import *
 
+
 class CreateCircle(Scene):
     def construct(self):
         circle = Circle()
         circle.set_fill(PINK, opacity=0.5)
         self.play(Create(circle))
 
+
 class SquareToCircle(Scene):
     def construct(self):
         circle = Circle()
         circle.set_fill(PINK, opacity=0.5)
 
-        square = Square
+        square = Square()
+        square.rotate(PI / 4)
+
+        self.play(Create(square))
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))
+
+
+class SquareAndCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
+
+        square = Square()
+        square.set_fill(BLUE, opacity=0.5)
+
+        square.next_to(circle, RIGHT, buff=0.5)
+        self.play(Create(circle), Create(square))
