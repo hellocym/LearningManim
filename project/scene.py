13c91a9df1023a21518ae77f8c7b2817248e79a9
hellocym <13681921379@163.com>
2022-11-18 11:56:24 +0000

    2

--- project/scene.py
+++ project/scene.py
@@ -5,3 +5,10 @@ class CreateCircle(Scene):
         circle = Circle()
         circle.set_fill(PINK, opacity=0.5)
         self.play(Create(circle))
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
+
+        square = Square
