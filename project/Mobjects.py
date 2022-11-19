378efcb2ea28c2a5dac3196e7fe714d876189e57
hellocym <13681921379@163.com>
2022-11-19 14:55:55 +0000

    2

--- project/Mobjects.py
+++ project/Mobjects.py
@@ -22,3 +22,17 @@ class Shapes(Scene):
 
         self.add(circle, square, triangle)
         self.wait(1)
+
+
+class MobjectPlacement(Scene):
+    def construct(self):
+        circle = Circle()
+        square = Square()
+        triangle = Triangle()
+
+        circle.move_to(LEFT * 2)
+        square.next_to(circle, LEFT)
+        triangle.align_to(circle, LEFT)
+
+        self.add(circle, square, triangle)
+        self.wait(1)
