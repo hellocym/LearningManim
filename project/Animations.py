68cdcb960e13ca075a474e0d20d7d8dcf393c487
hellocym <13681921379@163.com>
2022-11-21 04:28:55 +0000

    3

--- project/Animations.py
+++ project/Animations.py
@@ -32,3 +32,29 @@ class Runtime(Scene):
         self.add(square)
         self.play(square.animate.shift(UP), run_time=3)
         self.wait(1)
+
+
+class Count(Animation):
+    def __init__(self, number: DecimalNumber, start: float, end:float, **kwargs) -> None:
+        super().__init__(number, **kwargs)
+        self.start = start
+        self.end = end
+
+    def interpolate_mobject(self, alpha: float) -> None:
+        value = self.start + (alpha * (self.end - self.start))
+        self.mobject.set_value(value)
+
+
+class CountingScene(Scene):
+    def construct(self):
+        number = DecimalNumber().set_color(WHITE).scale(5)
+        number.add_updater(lambda number: number.move_to(ORIGIN))
+
+        self.add(number)
+
+        self.wait()
+
+        self.play(Count(number, 0, 100), run_time=4, rate_func=linear)
+
+        self.wait()
+
