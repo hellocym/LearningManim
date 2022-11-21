16c1a6341d7deb1dbf524358f9c088e135cb7480
hellocym <13681921379@163.com>
2022-11-21 12:11:08 +0000

    3

--- project/Animations.py
+++ project/Animations.py
@@ -1,3 +1,4 @@
+import numpy as np
 from manim import *
 
 
@@ -57,4 +58,28 @@ class CountingScene(Scene):
         self.play(Count(number, 0, 100), run_time=4, rate_func=linear)
 
         self.wait()
+
+
+class ExampleTransform(Scene):
+    def construct(self):
+        self.camera.background_color = WHITE
+        m1 = Square().set_color(RED)
+        m2 = Rectangle().set_color(RED).rotate(0.2)
+        self.play(Transform(m1, m2))
+
+
+class ExampleRotation(Scene):
+    def construct(self):
+        self.camera.background_color = WHITE
+        m1a = Square().set_color(RED).shift(LEFT)
+        m1b = Circle().set_color(RED).shift(LEFT)
+
+        m2a = Square().set_color(BLUE).shift(RIGHT)
+        m2b = Circle().set_color(BLUE).shift(RIGHT)
+
+        points = m2a.points
+        points = np.roll(points, int(len(points)/4), axis=0)
+        m2a.points = points
+
+        self.play(Transform(m1a, m1b), Transform(m2a, m2b), run_time=1)
 
